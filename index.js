f4fc54554d924bdc255d2a01443aadec040478fd
Nekero8 <nekero8@gmail.com>
2022-12-15 17:50:36 -0400

    Implemented GetCourses and GetCourse using graphql-tools and express-graphql

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 'use strict'
-const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
 const graphqlHTTP = require('express-graphql').graphqlHTTP
 const { readFileSync } = require('fs')
 const { join } = require('path')
-const resolvers = require('./lib/resolvers');
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
 // define Schema
-const schema = buildSchema(
-  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
-)
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
 
 app.use(
   '/api',
