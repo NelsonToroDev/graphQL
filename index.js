7544b9955d055a6a3e5410b73791750e3f2ac8f3
Nekero8 <nekero8@gmail.com>
2022-12-21 11:30:41 -0400

    Added Cors and cors-env to support production deployment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
-require('dotenv').config(); 
+require('dotenv').config();
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors');
 const graphqlHTTP = require('express-graphql').graphqlHTTP
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -9,6 +10,7 @@ const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 // define Schema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
@@ -17,12 +19,14 @@ const schema = makeExecutableSchema({
   resolvers,
 })
 
+app.use(cors())
+
 app.use(
   '/api',
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: isDev, // disable graphiQL for Development
   })
 )
 
